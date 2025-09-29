ee63e1ed7e2dca753ed2880959f2ba985f68e192
Mohsen <mohsenmgr@gmail.com>
2025-09-30 00:15:11 +0200

    fix sitemap generation logic

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,12 +2,36 @@
 module.exports = {
     siteUrl: process.env.NEXT_PUBLIC_DOMAIN_NAME || 'http://localhost:3000',
     generateRobotsTxt: true,
-    exclude: ['/amin/*'], // Exclude admin pages
+    exclude: ['/amin', '/amin/*'], // Exclude admin pages
     // Manually add /blog and dynamic posts
     additionalPaths: async (config) => {
         const paths = [
             {
                 loc: '/blog', // Main blog listing page
+                priority: 0.8,
+                changefreq: 'daily',
+                lastmod: new Date().toISOString()
+            },
+            {
+                loc: '/about', // Main blog listing page
+                priority: 0.8,
+                changefreq: 'weekly',
+                lastmod: new Date().toISOString()
+            },
+            {
+                loc: '/contact', // Main blog listing page
+                priority: 0.8,
+                changefreq: 'weekly',
+                lastmod: new Date().toISOString()
+            },
+            {
+                loc: '/feed', // Main blog listing page
+                priority: 0.8,
+                changefreq: 'daily',
+                lastmod: new Date().toISOString()
+            },
+            {
+                loc: '/work', // Main blog listing page
                 priority: 0.8,
                 changefreq: 'daily',
                 lastmod: new Date().toISOString()
